9ce55e15503f7d63b43bb2ab7adb03538c4b5a17
Derek Serrano <derek.joab@icloud.com>
2026-03-12 21:21:42 -0400

    add interactive demo

--- src/main/java/com/derekserrano/ratelimiter/controller/HomeController.java
+++ src/main/java/com/derekserrano/ratelimiter/controller/HomeController.java
@@ -6,16 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return """
-        Distributed Rate Limiter API
 
-        Test endpoint:
-        /api/test
-
-        Example:
-        curl -H "X-User-Id: user1" https://distributed-rate-limiter-7pn5.onrender.com/api/test
-        """;
-    }
 }
